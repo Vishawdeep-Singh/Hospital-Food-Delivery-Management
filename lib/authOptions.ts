c6b70b82e372d9095905549b63c1177e4cb1921a
Vishawdeep Singh <vishawdeepsingh29@gmail.com>
2025-01-07 22:00:59 +0530

    Init the app

---
+++ lib/authOptions.ts
@@ -0,0 +1,10 @@
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
